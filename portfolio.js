51b0de749c876e488486c390e3fa9f4c206baa91
Tom Jacobson <94944188+tomjacobson612@users.noreply.github.com>
2022-11-09 12:11:34 -0800

    Work history and project section
    Laid out skeleton for Work History and Projects sections using CSS grid. Added github and linkin links to navbar for large viewports.

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,7 @@
-// let body = document.querySelector("body");
+// let navlist = document.querySelector(".nav-list");
 
-// let about = document.querySelector(".about");
+// let github = navlist.createElement("li");
 
-// let str1 = `My name is Tom Jacobson and I am currently completing my master's degree in computer science at Portland State University, with a focus in software engineering. My previous background consists of a Bachelor of Business Administration from Western Washington University and time spent in retail management post graduation. \n`
-// let str2 = `During my time at Portland State I have learned a variety of different technologies. I would consider myself fluent in Python and have intermediate experience with C++, C, Java, and Javascript. This websited was designed using a combination of HTML, CSS, and Javascript. Other projects I have worked on include a discord trivia bot, fake news prediction program, and a sudoku solving program that generates a random sudoku board and solves it at the press of a button. Each of those projects were written in python. 
-// As I finish my degree I look forward continue bringing that same drive and excitement for learning to my professional career. The ability create applications that solve a problem for the user, where previously something did not exist, is an amazing feeling. I look forward to utilizing that passion for development in my career outside of the classroom.
-// I am currently focused gaining professional experience and on finishing my degree (estimated graduation date June 2024). Below you will find a brief history of my previous work and classes taken, as well as some links to projects I have worked on since beginning the computer science degree.`;
+// github.textContent = "Hi"
 
-// about.append(str1);
-// about.append(str2);
-
-// console.log(str1);
+// navlist.append(github);

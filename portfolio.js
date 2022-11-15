8b01c98a27cff4824ac5c53a28736b2e48688d70
Tom Jacobson <94944188+tomjacobson612@users.noreply.github.com>
2022-11-15 13:25:47 -0800

    Contact Form Added
    Added new page for contact form with relevant styling. Added new navbar functionality on contact form to navigate back to main page. Form made in bootstrap with responsiveness .

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,25 @@
-// let navlist = document.querySelector(".nav-list");
+// let index = 0;
+// let project_header = document.querySelector(".project-header");
 
-// let github = navlist.createElement("li");
+// function change_text_color(index, dom_element) {
+//   if (index > 2) {
+//     index = 0;
+//   }
+//   let list = ["red", "blue", "green"];
+//   console.log(index);
+//   dom_element.animate({ color: list[index] }, 5000),
+//     function () {
+//       dom_element.style.color = list[index];
+//     };
+  //   dom_element.style.color = list[index];
+  //   ++index;
+//   change_text_color(index + 1, dom_element);
+//   return index;
+// }
 
-// github.textContent = "Hi"
-
-// navlist.append(github);
+// change_text_color(0, document.querySelector(".project-header"));
+// setInterval(() => {
+//   index = change_text_color(index, document.querySelector(".project-header"));
+//   console.log(index);
+// }, 5000);
+// project_header.animate({}, 1000, change_text_color(index + 1, project_header));
